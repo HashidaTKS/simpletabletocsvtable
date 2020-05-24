c1c217938e3e9ce5459f59fd1214e86e8031823f
Takashi Hashida <t-hashida@amiya.co.jp>
2020-05-24 18:24:05 +0900

    Create minimal function

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Z_FILTERED } from 'zlib';
+import * as iconvLite from 'iconv-lite';
+import * as csvStringify from "csv-stringify/lib/sync";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,202 +13,215 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "simpletabletocsvtable" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('simpletabletocsvtable.helloworld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SimpleTableToCsvTable!');
-	});
-
 	let convert = vscode.commands.registerCommand('simpletabletocsvtable.convert', () => {
-		let editor = vscode.window.activeTextEditor; // エディタ取得
-		if(editor === undefined || editor === null){
+		const editor = vscode.window.activeTextEditor;
+		if (editor === undefined || editor === null) {
+			vscode.window.showInformationMessage('Please select a simple table');
 			return;
 		}
-		let doc = editor.document;            // ドキュメント取得
-		let cur_selection = editor.selection; // 選択範囲取得
-		if(editor.selection.isEmpty){         
-			// 選択範囲が空であれば全てを選択範囲にする
-			let startPos = new vscode.Position(0, 0);
-			let endPos = new vscode.Position(doc.lineCount - 1, 10000);
-			cur_selection = new vscode.Selection(startPos, endPos);
+		const doc = editor.document;
+		const cur_selection = editor.selection;
+		if (editor.selection.isEmpty) {
+			vscode.window.showInformationMessage('Please select a simple table');
+			return;
 		}
-		
-		let text = doc.getText(cur_selection); //取得されたテキスト
-		
-		var convertor = new Convertor('=== =====\n\
-		aaa bbbbb\n\
-		=== =====\n\
-		aaa ccccc\n\
-		あa いうd\n\
-		=== =====');
-		
-		
-		//エディタ選択範囲にテキストを反映
+
+		const text = doc.getText(cur_selection);
+		const convertor = new Convertor(text);
+		if (!convertor.isValidFormat()) {
+			vscode.window.showErrorMessage('Invalid format');
+			return;
+		}
 		editor.edit(edit => {
-			edit.replace(cur_selection, text);
+			edit.replace(cur_selection, convertor.getCsvTable());
 		});
 	});
 
-
-
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(convert);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
 
-export class Convertor{
-	columnCount : number = 0;
-	headerBlock : string[] = [];
-	bodys : string[] = [];
-	columnLengths : number[] = [];
-	tableBorder : string = "";
-	validFormat : boolean = false;
+export class Convertor {
+	private validFormat: boolean = false;
+	private headerStringArray: string[] = [];
+	private bodyStringArray: string[] = [];
+	private columnStartEndTable: any[] = [];
+	private tableBorder: string = "";
 
-    constructor(input_str: string){
-		let inputLines : string[] = input_str.split('\n').map(x => x.trim()).filter(x => x.trim() !== "");
-		this.validFormat = this.checkFormatAndSetTableBorder(inputLines);
-		if(!this.validFormat){
+	constructor(input_str: string) {
+		let inputLines: string[] = input_str.split('\n').map(x => x.trim()).filter(x => x.trim() !== "");
+		this.checkFormat(inputLines);
+		if (!this.validFormat) {
 			return;
 		}
-		const blockIterator = this.splitHeaderAndBody(inputLines);
-		this.headerBlock = blockIterator.next().value as string[];
-		this.bodys = blockIterator.next().value as string[];
-		this.get_cell_of_line(this.bodys[1],1);
+		this.tableBorder = inputLines[0];
+		this.setHeaderAndBodyStringArrays(inputLines);
+		this.setColumnStartEndTable();
 	}
 
-	checkFormatAndSetTableBorder(inputLines : string[])
-	{
-		if(inputLines.length < 5){
-			return false;
+	public isValidFormat(){
+		return this.validFormat;
+	}
+
+	private setColumnStartEndTable() {
+		const columnLengthArray = this.tableBorder.replace(/  +/g, " ").split(' ').map(x => x.length);
+		for (let columnIndex = 0; columnIndex < columnLengthArray.length; columnIndex++) {
+			let start = 0;
+			for (let i = 0; i < columnIndex; i++) {
+				start += columnLengthArray[i];
+				//count and add spaces after this column
+				while (this.tableBorder.charAt(start) === " ") {
+					start++;
+				}
+			}
+			let end = start + columnLengthArray[columnIndex];
+			this.columnStartEndTable.push({ "start": start, "end": end });
 		}
-		if(!inputLines[0].match(/=+( =+)+/)){
-			return false;
+	}
+
+	private checkFormat(inputLines: string[]) {
+		this.validFormat = false;
+		if (inputLines.length < 5) {
+			return;
 		}
-		this.tableBorder = inputLines[0];
-		let beforeLine = this.tableBorder;
+		if (!inputLines[0].match(/=+( =+)+/)) {
+			return;
+		}
+		const tableBorder = inputLines[0];
+		let beforeLine = tableBorder;
 		let borderCount = 1;
-		for(let i = 1; i < inputLines.length; i++){
-			if(inputLines[i] === this.tableBorder){
-				if(beforeLine === this.tableBorder){
-					return false;
+		for (let i = 1; i < inputLines.length; i++) {
+			if (inputLines[i] === tableBorder) {
+				if (beforeLine === tableBorder) {
+					return;
 				}
 				borderCount++;
 			}
-			if(i === inputLines.length - 1){
-				if(inputLines[i] !== this.tableBorder){
-					return false;
+			if (i === inputLines.length - 1) {
+				if (inputLines[i] !== tableBorder) {
+					return;
 				}
 			}
 			beforeLine = inputLines[i];
 		}
-		if(borderCount !== 3){
-			return false;
+		if (borderCount !== 3) {
+			return;
 		}
-		this.columnLengths = this.tableBorder.replace(/  +/g, " ").split(' ').map(x => x.length);
-		return true;
+		this.validFormat = true;
 	}
 
-	*splitHeaderAndBody(inputLines : string[])
-	{
-		let header : string[] = [];
-		let body : string[] = [];
+	private setHeaderAndBodyStringArrays(inputLines: string[]) {
 		let borderCount = 0;
-		for(let line of inputLines){
-			if(line === this.tableBorder){
+		for (const line of inputLines) {
+			if (line === this.tableBorder) {
 				borderCount++;
 				continue;
 			}
-			switch(borderCount){
+			switch (borderCount) {
 				case 1:
-					header.push(line);
+					this.headerStringArray.push(line);
 					break;
 				case 2:
-					body.push(line);
+					this.bodyStringArray.push(line);
 					break;
 				default:
 					continue;
 			}
 		}
-		yield header;
-		yield body;
 	}
 
-    get_record_list_from_line_str(line_str : string){
-        let record : string[] = [];
-        for(let i = 0; i < this.columnLengths.length; i++){
-            //get_cell_of_lineを毎回呼び出すのは数えなおしが発生して少し無駄だが、PFM要求されるスクリプトでもないのでいいか
-			record.push(this.get_cell_of_line(line_str, i).trim());
+	private getRecordArrayFromLineStr(lineStr: string) {
+		const record: string[] = [];
+		for (let i = 0; i < this.columnStartEndTable.length; i++) {
+			//blank cell is converted to single space cell
+			record.push(this.getColumnValueOfLine(lineStr, i).trimRight().replace(/^\\$/, ' '));
 		}
 		return record;
 	}
 
-    get_cell_of_line(line_str: string, column_index: number){
-        let start = 0;
-        for(let i = 0; i < column_index; i++){
-			start += this.columnLengths[i];
-			//count and add spaces after this column
-			let j = start;
-			while(this.tableBorder[++j] === " "){
-				++start;
+	private getColumnValueOfLine(lineStr: string, columnIndex: number) {
+		const startEndObj = this.columnStartEndTable[columnIndex];
+		const start = startEndObj["start"] as number;
+		//used by slice(), so add 1
+		let end = startEndObj["end"] as number + 1;
+
+		//support japanese
+		//regarding multibyte char as 2bytes
+		//TODO: support other languages
+		let encodedLine = iconvLite.encode(lineStr, "windows932");
+		if ((columnIndex === this.columnStartEndTable.length - 1) && (encodedLine.length > end)) {
+			//only at the last column, the value length can be over the border length 
+			//because there is no column separater after the last column.
+			end = encodedLine.length;
+		}
+		return iconvLite.decode(encodedLine.slice(start, end), "windows932");
+	}
+
+	private *getCsvRecords() {
+		//header
+		//not support multi line header
+		yield this.getRecordArrayFromLineStr(this.headerStringArray[0]);
+		
+		//body
+		const bodyRecordArray: [string[]] = [] as unknown as [string[]];
+		for (const bodyStr of this.bodyStringArray) {
+			bodyRecordArray.push(this.getRecordArrayFromLineStr(bodyStr));
+		}
+
+		const mergedRecordArray: [string[]] = [] as unknown as [string[]];
+		let lineBuffer: [string[]] = [] as unknown as [string[]];
+		for (const body of bodyRecordArray.reverse()) {
+			lineBuffer.unshift(body);
+			if (body[0] === "") {
+				continue;
+			}
+			else {
+				mergedRecordArray.unshift(this.mergeBuffer(lineBuffer));
+				lineBuffer = [] as unknown as [string[]];
 			}
 		}
-		let end = start + this.columnLengths[column_index];
-		
-		//utf-8にすると一文字3byte以上のケースで上手くいかないのでcp932にしておく
-		let x = Buffer.from(line_str, 'ascii');
-		let y = Buffer.from(line_str, 'ascii').slice(start, end);
-		let z = Buffer.from(line_str, 'ascii').slice(start, end).toString('utf-8');
-		return Buffer.from(line_str).slice(start, end).toString('utf-8');
+		for (const mergedRecord of mergedRecordArray) {
+			yield mergedRecord;
+		}
 	}
 
-	/*
-    get_csv_records(){
-        header = self.get_record_list_from_line_str(this.headerBlock[1])
-        bodys = [self.get_record_list_from_line_str(x) for x in this.bodys]
-
-        yield header
-        # 複数行のレコードをマージする
-        # 逆順に見ていくことでいい感じにやる
-        # 一番左側が空かどうかで見る
-        result_bodys = []
-        one_line_buffer = []
-        for body in reversed(bodys):
-            one_line_buffer.insert(0, body)
-            if(body[0] == ""):
-                continue
-            else:
-                result_bodys.insert(0, self.combine_buffer(one_line_buffer))
-                one_line_buffer = []
-        for result_body in result_bodys:
-			yield result_body
+	private mergeBuffer(lineBuffer: [string[]]) {
+		if (!lineBuffer || !lineBuffer.length) {
+			return [];
+		}
+		if (lineBuffer.length === 1) {
+			return lineBuffer[0];
+		}
+		const records: [string[]] = [] as unknown as [string[]];
+		for (const record of lineBuffer) {
+			records.push(record.map(x => `| ${x}`));
+		}
+		const result: string[] = [];
+		for (let i = 0; i < records[0].length; i++) {
+			const i_values: string[] = [];
+			for (const record of records) {
+				i_values.push(record[i]);
+			}
+			result.push(i_values.join("\n"));
+		}
+		return result;
 	}
 
-    combine_buffer(one_line_buffer){
-        # 連続するレコードを一つのレコードに変換する。
-        if len(one_line_buffer) == 0:
-            return []
-        if len(one_line_buffer) == 1:
-            return one_line_buffer[0]
-        records = []
-        for record in one_line_buffer:
-            records.append(list(map(lambda x: "| " + x, record)))
-        result = []
-        for i in range(len(records[0])):
-            # 組み込みだけで行いたいのでreduceを使っていない
-            i_values = []
-            for record in records:
-                i_values.append(record[i])
-            result.append("\n".join(i_values))
-		return result
-	}
+	getCsvTable() {
+		if(!this.isValidFormat()){
+			return "";
+		}
+		let result: string = "";
+		result += '.. csv-table::\n';
+		result += '  :header-rows: 1\n';
+		result += '  \n';
 
-    output_csv_table(){
+		for (const data of this.getCsvRecords()) {
+			//add each record respectively in order to add left padding spaces.
+			result += `  ${csvStringify([data], { header: false })}`;
+		}
+		return result;
 	}
-	*/
 }
